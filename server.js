218767b6687309e7e1494ac5e4d28f40b584c219
vaguilar25 <vivian.aguilar@gmail.com>
2018-12-20 23:29:09 -0600

    change port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var path = require("path");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8050;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
